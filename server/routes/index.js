91eea8c37dfed56ad64234c15ab05fe4b3e4782a
Abdullah <itsabdullah80@gmail.com>
2024-05-16 05:15:00 +0300

    edit middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,14 @@
 const express=require('express')
 const router=express.Router()
 const mainController=require('../controllers/mainController')
-const auth=require('../controllers/auth')
+const {authenticated}=require('../middleware/auth')
 
 // app routes
 
 router.get('/', mainController.homepage)
-router.get('/signup', mainController.signup)
-router.get('/signin', mainController.signin)
-router.get('/reset', mainController.reset)
+router.get('/signup', authenticated, mainController.signup)
+router.get('/signin', authenticated, mainController.signin)
+router.get('/reset', authenticated, mainController.reset)
 
 
 
